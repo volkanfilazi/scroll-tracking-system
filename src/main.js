d478dff70cc125a00a569498ccbc41cc160977ff
volkan filazi <74497449+volkanfilazi@users.noreply.github.com>
2023-06-02 22:49:13 +0200

    License added

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import LearnVueButton from "volki-button"
 
-createApp(App).mount('#app')
+createApp(App).use(LearnVueButton).mount('#app')
